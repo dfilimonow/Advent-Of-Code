66d9375958e26711605de37acc3f792573f3cd63
Daniel F <dan.filimonow@gmail.com>
2023-12-01 20:41:39 +0100

    Day1

--- 2023/Day1/solution.hs
+++ 2023/Day1/solution.hs
@@ -2,30 +2,25 @@ import Data.Char ( isDigit )
 import Data.Text.Internal.Search ( indices )
 import Data.Text (unpack, pack, Text)
 import Control.Monad (liftM2)
-import Data.List ( elemIndex )
 import Data.Maybe (fromMaybe)
+import Data.List (isPrefixOf, elemIndex )
 
-tokens :: [Text]
-tokens = fmap pack ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+tokens :: [String]
+tokens = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+findTokens :: String -> [String]
+findTokens s@(x:xs) = foldr (\x acc -> if x `isPrefixOf` s then x : acc else acc) [] tokens ++ findTokens xs
+findTokens [] = []
 
 customRead :: String -> Int
-customRead val@(_:_:_) = fromMaybe undefined (pack val `elemIndex` tokens) + 1
+customRead val@(_:_:_) = fromMaybe undefined (val `elemIndex` tokens) + 1
 customRead val = read val
 
-findTokens :: Text -> [(String, Int)]
-findTokens input = concatMap (\x -> liftM2 (,) [unpack x] (indices x input)) tokens
-
-findValue :: [(String, Int)] -> Int
-findValue records =
-        let (maxDigit, _) = foldr (\x acc -> if snd x > snd acc then x else acc) ("", -1) records in
-        let (minDigit, _) = foldr (\x acc -> if snd x < snd acc then x else acc) ("", 100000) records in
-        customRead minDigit * 10 + customRead maxDigit
-
 solutionOne :: [String] -> Int
-solutionOne = sum . fmap ((read :: String -> Int) . (\digits -> [head digits, last digits]) . filter isDigit)
+solutionOne = sum . fmap (read . (\digits -> [head digits, last digits]) . filter isDigit)
 
 solutionTwo :: [String] -> Int
-solutionTwo = sum . fmap (findValue . findTokens . pack)
+solutionTwo = sum . fmap ((\tokens -> customRead (head tokens) * 10 + customRead (last tokens)) . findTokens)
 
 main :: IO()
 main = do
