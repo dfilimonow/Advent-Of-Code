f983b649f730c3fb0d2885e79722fd77e982842f
Daniel F <dan.filimonow@gmail.com>
2023-12-09 16:00:22 +0100

    Day9

--- 2023/Day9/solution.d
+++ 2023/Day9/solution.d
@@ -3,28 +3,21 @@ import std.ascii;
 import std.algorithm;
 import std.conv;
 import std.array;
+import std.range;
 
-int[][] parseInput(File input) => input.byLine!(string)(KeepTerminator.no, std.ascii.newline).map!(a => a.split.map!(to!int).array).array;
+int[][] parseInput(File input) => input.byLine!string(KeepTerminator.no, std.ascii.newline).map!(a => a.split.map!(to!int).array).array;
 
-int[] reduce(int[] source)
-{   
-    int[] target;
-    foreach(i; 1..source.length)
-    {
-        target ~= source[i] - source[i - 1];
-    }
-    return target;
-}
+int[] reduce(int[] source) => source.zip(source.dropOne).map!(t => t[1] - t[0]).array;
 
-int extrapolate(int[] sequence) => all!"a == 0"(sequence) ? 0 : sequence[$ - 1] + sequence.reduce.extrapolate;
+int extrapolate(int[] sequence) => all!"a == 0"(sequence) ? 0 : sequence.back + sequence.reduce.extrapolate;
 
-int extrapolateBackwards(int[] sequence) => all!"a == 0"(sequence) ? 0 : sequence[0] + sequence.reverse.reduce.extrapolate;
+int extrapolateBackwards(int[] sequence) => all!"a == 0"(sequence) ? 0 : sequence.front + sequence.reverse.reduce.extrapolate;
 
 int solutionEasy(int[][] histories) =>
-    histories.map!(extrapolate).sum;
+    histories.map!extrapolate.sum;
 
 int solutionHard(int[][] histories) =>
-    histories.map!(extrapolateBackwards).sum;
+    histories.map!extrapolateBackwards.sum;
 
 void main()
 {
